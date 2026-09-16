fad82ad4daed27e6daa87b202340608984927945
talelhanin <151065931+talelhanin@users.noreply.github.com>
2026-09-16 08:15:47 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""Course-project package exports."""
-
-from emotion_detection import EmotionServiceError, emotion_detector, emotion_predictor
-
-__all__ = ["EmotionServiceError", "emotion_detector", "emotion_predictor"]
